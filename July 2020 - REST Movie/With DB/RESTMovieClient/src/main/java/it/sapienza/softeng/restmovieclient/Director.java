fd5d3cc4db5b96873b00a861b661edf5285d2eb3
Chiara Esposito <48214920+ChiaraEsposito@users.noreply.github.com>
2021-01-21 18:46:36 +0100

    Add files via upload

---
+++ July 2020 - REST Movie/With DB/RESTMovieClient/src/main/java/it/sapienza/softeng/restmovieclient/Director.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.sapienza.softeng.restmovieclient;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+/**
+ *
+ * @author studente
+ */
+@JacksonXmlRootElement(localName = "Director")
+public class Director {
+    
+    private int id;
+    private String name, yearOfBirth;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getBirth() {
+        return yearOfBirth;
+    }
+
+    public void setBirth(String yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    @Override
+    public int hashCode() {
+        return id + name.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Director) && (id == ((Director) obj).getId()) && (name.equals(((Director) obj).getName()));
+    }
+    @Override
+    public String toString() {
+        return "Director: " + name + " " + yearOfBirth;
+    }
+    
+}
