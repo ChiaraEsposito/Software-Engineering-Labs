b9f127473b0510ef8821e3ef2ae8df571994eb21
Chiara Esposito <48214920+ChiaraEsposito@users.noreply.github.com>
2021-02-13 23:21:21 +0100

    Add files via upload

---
+++ January20/JMSSubscriber/src/main/java/it/sapienza/softeng/jmssubscriber/ProfessorsClient.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.sapienza.softeng.jmssubscriber;
+
+/**
+ *
+ * @author studente
+ */
+public class ProfessorsClient {
+    public static void main(String[] args) {
+        
+        Professors p = new Professors();
+        p.start();
+        
+    }
+       
+}
